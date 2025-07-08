1e11348552c9e7fac6842c9408c1069f01bba6e9
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-08 13:13:36 -0300

    add endpoints

--- src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioController.java
+++ src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioController.java
@@ -1,15 +1,53 @@
 package java10x.devnoah.apicadastro.Usuario;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("usuario")
 public class UsuarioController {
+
+    private UsuarioService usuarioService;
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota!";
     }
+
+    //Add
+    @PostMapping("/criar")
+    public String criarUsuario(){
+        return "Usuário criado com sucesso!";
+    }
+
+    //Delete
+    @DeleteMapping("/deletar/id")
+    public String deletarUsuario(){
+        return "Usuario deletado";
+    }
+
+    //read-return
+    @GetMapping("/listar")
+    public List<UsuarioModel> listar(){
+        return usuarioService.listar();
+    }
+
+    //read id - return id
+    @GetMapping("/listar/id")
+    public String listarporid(){
+        return "Lista de usuário por id";
+    }
+
+    //edit - update
+    @PutMapping("/editar/id")
+    public String editarUsuario() {
+        return "Usuário editado com sucesso!";
+    }
+
+
 }
+
