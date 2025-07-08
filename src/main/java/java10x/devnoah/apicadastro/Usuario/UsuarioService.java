db167829d79bfeb66427a3a9f11eeae99e137fb0
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-08 13:12:17 -0300

    add a service logic

--- src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioService.java
+++ src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioService.java
@@ -1,4 +1,18 @@
 package java10x.devnoah.apicadastro.Usuario;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<UsuarioModel> listar() {
+        return usuarioRepository.findAll();
+    }
 }
