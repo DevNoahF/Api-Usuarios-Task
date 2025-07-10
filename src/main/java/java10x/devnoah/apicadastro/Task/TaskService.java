4b35944c9fac61eac0fd74e47f04529d7ef3b67f
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-10 13:14:14 -0300

    add: logica de serviço adicionada (criar, listar, listar por id)

--- src/main/java/java10x/devnoah/apicadastro/Task/TaskService.java
+++ src/main/java/java10x/devnoah/apicadastro/Task/TaskService.java
@@ -1,4 +1,33 @@
 package java10x.devnoah.apicadastro.Task;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    // Método para criar uma nova tarefa
+    public TaskModel criarTask(TaskModel taskModel) {
+        return taskRepository.save(taskModel);
+    }
+
+    // listar
+    public List<TaskModel> listar(){
+        return taskRepository.findAll();
+    }
+
+    // listar por id
+    public TaskModel listarPorId(Long id) {
+        Optional<TaskModel> taskModel = taskRepository.findById(id);
+        return taskRepository.findById(id).orElse(null);
+    }
+
+
 }
