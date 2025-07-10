2335bfd8032c1a46fff07e3765e9a1c3dbf7f454
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-10 12:54:21 -0300

    create: logica de serviço adicionada (listar por id, listar, adicionar)

--- src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioService.java
+++ src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioService.java
@@ -3,16 +3,30 @@ package java10x.devnoah.apicadastro.Usuario;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
+    // Construtor para injeção de dependência do repositório
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
 
+    // Método para listar todos os usuários
     public List<UsuarioModel> listar() {
         return usuarioRepository.findAll();
+    }
+
+    // Método para criar um novo usuário
+    public UsuarioModel criarUser(UsuarioModel usuarioModel){
+        return usuarioRepository.save(usuarioModel);
+    }
+
+    // Método para deletar um usuário por ID
+    public UsuarioModel listarPorId(Long id){
+        Optional<UsuarioModel> usuario = usuarioRepository.findById(id);
+        return usuario.orElse(null);
     }
 }
