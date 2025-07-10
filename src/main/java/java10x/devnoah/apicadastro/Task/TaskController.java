4aa3a50e61269180c6fbf7ef7a15c0cde9b6c110
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-10 13:14:48 -0300

    add: controller adicionado (criar, listar, listar por id)

--- src/main/java/java10x/devnoah/apicadastro/Task/TaskController.java
+++ src/main/java/java10x/devnoah/apicadastro/Task/TaskController.java
@@ -1,5 +1,40 @@
 package java10x.devnoah.apicadastro.Task;
 
+import java10x.devnoah.apicadastro.Usuario.UsuarioModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
+    @PostMapping("/criar")
+    public TaskModel criarTask(@RequestBody TaskModel taskModel){
+        return taskService.criarTask(taskModel);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletar(){
+        return "tarefa deletada";
+    }
+
+    @GetMapping("/listar")
+    public List<TaskModel> listar(){
+        return taskService.listar();
+    }
+
+    @GetMapping("/listar/{id}")
+    public TaskModel listarPorId(@PathVariable Long id){
+        return taskService.listarPorId(id);
+    }
+
+
 
 }
