802f37e2aed6e63f331a109b2baeddf844c0b416
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-15 11:09:40 -0300

    feat: dto config add entities

--- src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioDTO.java
+++ src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioDTO.java
@@ -1,4 +1,18 @@
 package java10x.devnoah.apicadastro.Usuario;
 
+import java10x.devnoah.apicadastro.Task.TaskModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String sexo;
+    private TaskModel task;
 }
