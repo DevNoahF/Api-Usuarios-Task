2d0bd3cf15d1d8880ca74b486142a40b32a2ae4a
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-15 11:10:21 -0300

    feat: config mapper

--- src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioMapper.java
+++ src/main/java/java10x/devnoah/apicadastro/Usuario/UsuarioMapper.java
@@ -1,4 +1,28 @@
 package java10x.devnoah.apicadastro.Usuario;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+    public UsuarioModel map(UsuarioDTO usuario){
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setId(usuario.getId());
+        usuarioModel.setNome(usuario.getNome());
+        usuarioModel.setEmail(usuario.getEmail());
+        usuarioModel.setIdade(usuario.getIdade());
+        usuarioModel.setSexo(usuario.getSexo());
+        usuarioModel.setTask(usuario.getTask());
+        return usuarioModel;
+    }
+
+    public UsuarioDTO map(UsuarioModel usuario){
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(usuario.getId());
+        usuarioDTO.setNome(usuario.getNome());
+        usuarioDTO.setEmail(usuario.getEmail());
+        usuarioDTO.setIdade(usuario.getIdade());
+        usuarioDTO.setSexo(usuario.getSexo());
+        usuarioDTO.setTask(usuario.getTask());
+        return usuarioDTO;
+    }
 }
