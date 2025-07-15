1e77793436da6223e8df7b909745390eb5e0579a
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-15 12:41:15 -0300

    feat: configuração do dto e mapper

--- src/main/java/java10x/devnoah/apicadastro/Task/TaskMapper.java
+++ src/main/java/java10x/devnoah/apicadastro/Task/TaskMapper.java
@@ -1,4 +1,26 @@
 package java10x.devnoah.apicadastro.Task;
+import org.springframework.stereotype.Component;
 
+@Component // Use @Component to allow Spring to manage this class as a bean
 public class TaskMapper {
+    public TaskModel map(TaskDTO task) {
+        TaskModel taskModel = new TaskModel();
+        taskModel.setId(task.getId());
+        taskModel.setNome(task.getNome());
+        taskModel.setDescricao(task.getDescricao());
+        taskModel.setUsuarios(task.getUsuarios());
+        return taskModel;
+    }
+    public TaskDTO map(TaskModel task){
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId(task.getId());
+        taskDTO.setNome(task.getNome());
+        taskDTO.setDescricao(task.getDescricao());
+        taskDTO.setUsuarios(task.getUsuarios());
+        return taskDTO;
+    }
+
+
+
+
 }
