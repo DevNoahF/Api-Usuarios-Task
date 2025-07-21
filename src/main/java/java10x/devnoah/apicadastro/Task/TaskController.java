7c890e313bfaeaed942fbd57141b626cdc9131ef
Noah Vêri Franco <noahvf16@outlook.com>
2025-07-21 10:20:29 -0300

    feat: refact ( listar por id, listar, deletar, editar, criar) to ResponseEntity task

--- src/main/java/java10x/devnoah/apicadastro/Task/TaskController.java
+++ src/main/java/java10x/devnoah/apicadastro/Task/TaskController.java
@@ -1,5 +1,9 @@
 package java10x.devnoah.apicadastro.Task;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,28 +19,50 @@ public class TaskController {
 
 
     @PostMapping("/criar")
-    public TaskDTO criarTask(@RequestBody TaskDTO taskDTO) {
-        return taskService.criarTask(taskDTO);
+    public ResponseEntity<String> criarTask(@RequestBody TaskDTO taskDTO) {
+        TaskDTO usuario = taskService.criarTask(taskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Usuario criado!");
     }
 
     @DeleteMapping("/deletar/{id}")
-    public void deletar(@PathVariable Long id){
-        taskService.deletarTaskId(id);
+    public ResponseEntity<String> deletar(@PathVariable Long id){
+
+        if (taskService.listarPorId(id) != null){
+            taskService.deletarTaskId(id);
+            return ResponseEntity.ok("usuario deletado");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuario de ID: " + id + " nao encontrado!");
+        }
     }
 
     @GetMapping("/listar")
-    public List<TaskDTO> listar(){
-        return taskService.listar();
+    public ResponseEntity<List<TaskDTO>> listar(){
+        List<TaskDTO> task =  taskService.listar();
+        return ResponseEntity.ok(task);
     }
 
     @GetMapping("/listar/{id}")
-    public TaskDTO listarPorId(@PathVariable Long id){
-        return taskService.listarPorId(id);
+    public ResponseEntity<?> listarPorId(@PathVariable Long id){
+        TaskDTO task = taskService.listarPorId(id);
+        if (task != null){
+            return ResponseEntity.ok(task);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Task de ID: " + id + " nao encontrada!");
+        }
     }
 
     @PutMapping("/atualizar/{id}")
-    public TaskDTO atualizar(@PathVariable Long id, @RequestBody TaskDTO task){
-        return taskService.atualizarPorId(id,task);
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody TaskDTO taskDTO){
+        TaskDTO task = taskService.atualizarPorId(id,taskDTO);
+        if (task != null){
+            return ResponseEntity.ok(task);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Task com ID: " + id + " nao foi encontrado");
+        }
     }
 
 
